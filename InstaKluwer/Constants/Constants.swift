ed28b05b21f86d16e43b5e3d7d0c752a719dbe9f
LonestarX <me@lonestarx.net>
2022-11-09 02:33:18 +0200

    Update Constants.swift

--- InstaKluwer/Constants/Constants.swift
+++ InstaKluwer/Constants/Constants.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class Constants: NSObject {
     static let baseURL = "https://graph.instagram.com/"
-    static let accessToken = "access_token_here"
+    static let accessToken = access_token_here
     static let albumsURL = "\(baseURL)me/media?fields=media_url,timestamp,caption&access_token=\(Constants.accessToken)"
     static let photosURLComponent = "/children?fields=id,media_url,timestamp&access_token=\(Constants.accessToken)"
 }
