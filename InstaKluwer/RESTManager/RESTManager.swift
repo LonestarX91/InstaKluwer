f23d39f640eee952b168abf93c45a297ec4ef7de
LonestarX <me@lonestarx.net>
2022-11-09 16:35:33 +0200

    refactored RESTManager a bit to eliminate code duplication

--- InstaKluwer/RESTManager/RESTManager.swift
+++ InstaKluwer/RESTManager/RESTManager.swift
@@ -26,63 +26,60 @@ class RESTManager {
       session = URLSession(configuration: config)
     }
     
-    func getAlbumsData(completion: @escaping ([Photo]?, Error?) -> (Void)) {
-    let request = URLRequest(url: URL(string: Constants.albumsURL)!)
-      let task = session.dataTask(with: request) { data, response, error in
-        if let error = error {
-          completion(nil, error)
-          return
-        }
-          guard let data = data else {
-            completion(nil, RESTError.dataError)
-            return
+    func makeRequest(url: URL, completion: @escaping (Data?, Error?) -> (Void)) {
+        let request = URLRequest(url: url)
+          let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+              completion(nil, error)
+              return
+            }
+              guard let data = data else {
+                completion(nil, RESTError.dataError)
+                return
+              }
+              completion(data, nil)
           }
-          
-        do {
-            let group = DispatchGroup()
-            let response = try JSONDecoder().decode(APIResponse.self, from: data)
-            for album in response.data {
-                group.enter()
-                if let url = URL(string: Constants.baseURL + album.id + Constants.photosURLComponent) {
-                    self.getAlbumPhotos(url: url) { photo, error in
-                        album.children = photo ?? []
-                        group.leave()
+          task.resume()
+    }
+    
+    func getAlbumsData(completion: @escaping ([Photo]?, Error?) -> (Void)) {
+        makeRequest(url: URL(string: Constants.albumsURL)!) { data, error in
+            if let data = data {
+                do {
+                    let group = DispatchGroup()
+                    let response = try JSONDecoder().decode(APIResponse.self, from: data)
+                    for album in response.data {
+                        group.enter()
+                        if let url = URL(string: Constants.baseURL + album.id + Constants.photosURLComponent) {
+                            self.getAlbumPhotos(url: url) { photo, error in
+                                album.children = photo ?? []
+                                group.leave()
+                            }
+                        }
                     }
+                    group.notify(queue: DispatchQueue.main) {
+                        completion(response.data, nil)
+                    }
+                } catch let error {
+                    print("ERROR \(error)")
+                    completion(nil, error)
                 }
             }
-            group.notify(queue: DispatchQueue.main) {
-                completion(response.data, nil)
-            }
-        } catch let error {
-            print("ERROR \(error)")
-          completion(nil, error)
+            completion(nil, error)
         }
-      }
-      
-      task.resume()
     }
     
     func getAlbumPhotos(url: URL, completion: @escaping ([Photo]?, Error?) -> (Void)) {
-        let request = URLRequest(url: url)
-        let task = session.dataTask(with: request) { data, response, error in
-          if let error = error {
-            completion(nil, error)
-            return
-          }
-            guard let data = data else {
-              completion(nil, RESTError.dataError)
-              return
+        makeRequest(url: url) { data, error in
+            if let data = data {
+                do {
+                    let response = try JSONDecoder().decode(APIResponse.self, from: data)
+                    completion(response.data, nil)
+                } catch let error {
+                    print("ERROR \(error)")
+                    completion(nil, error)
+                }
             }
-            
-          do {
-              let response = try JSONDecoder().decode(APIResponse.self, from: data)              
-              completion(response.data, nil)
-          } catch let error {
-              print("ERROR \(error)")
-            completion(nil, error)
-          }
         }
-        
-        task.resume()
     }
 }
